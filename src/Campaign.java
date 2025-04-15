a617649dbf55dc70fa4cf6b611d7e5b87ea4e045
noah-the-kang <noahkang82@gmail.com>
2025-04-15 09:56:31 -0500

    plotting ideas

---
+++ src/Campaign.java
@@ -0,0 +1,16 @@
+public class Campaign{
+    int[] defenders;
+    int attrackers;
+
+    public Campaign(int[] defenders, int attackers){
+        this.defenders=defenders;
+        this.attrackers=attackers;
+    }
+
+    /**
+     * Simulates a campaign with the given number of defenders and attackers.
+     * The simulation runs for a specified number of rounds, where each annexing territory's
+     * number of defenders is specified by the left-most element of the defenders array.
+     */
+     public void simulateCampaign(){}
+}
